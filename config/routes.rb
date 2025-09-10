1016dc61f46243fa57e779746b8957967a093eb3
Karthik Chitta <kschitta@tamu.edu>
2025-09-09 22:20:45 -0500

    book collection 2 repo is filled with book collection 1 app info

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :books do
+    member do
+      get :delete   # confirmation page before destruction
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "books#index"
+end
